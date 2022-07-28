00e195a89162f43b6377ee89fad536f883f206d1
Fran Linde Blazquez <franlindebl@gmail.com>
2022-07-28 10:29:16 +0200

    Tested some JSX advantages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Perico el de los palotes";
+  const hour = 9;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hola!</h1>
+      <p className="sample">Esto es un ejemplo</p>
+      <p>Mi nombre es: {name}</p>
+      <p>La suma de 100 y 371 es: {100 + 371}</p>
+      { hour > 12 ? <p>Buenas tardes!</p> : <p>Buenos días!</p> }
+    </>
   );
 }
 
